f06966e688e2409a90b5b2a2a099317176612010
ifyouwanagoa <42785783+ifyouwanagoa@users.noreply.github.com>
2018-12-26 15:17:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,33 +7,9 @@ const client = new Discord.Client();
 client.on('ready', () => {                           
 client.user.setGame(`♥ Premium ℕova ♥ `,'https://www.twitch.tv/ℕ o v a Codes♥');
   });
+  READ_MESSAGES: false
 
-client.on('message', message => {
-var prefix = "-";
-       if(message.content === prefix + "hc") {
-                           if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
-              message.channel.overwritePermissions(message.guild.id, {
-            READ_MESSAGES: false
-
-              }).then(() => {
-                  message.reply("**__تم احفاء الشات__ ✅ **")
-              });
-                }
-
-    if(message.content === prefix + "sc") {
-                        if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
-              message.channel.overwritePermissions(message.guild.id, {
-            READ_MESSAGES: true
-
-              }).then(() => {
-                  message.reply("**__تم اظهار الشات__✅**")
-              });
-    }
-       
+   }
 });
 
 client.login(process.env.BOT_TOKEN);
